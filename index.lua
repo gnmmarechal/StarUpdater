810e54769f757d75b83cc46f2447f3bb18b8f451
gnmmarechal <gnmpolicemata91@gmail.com>
2016-07-23 17:35:24 +0100

    Started work on the rewrite, variables are prettier now :)

--- index.lua
+++ index.lua
@@ -1,26 +1,58 @@
-local white = Color.new(255,255,255)
-local yellow = Color.new(255,205,66)
-local red = Color.new(255,0,0)
-local green = Color.new(55,255,0)
-local hourlyUrl = "http://astronautlevel2.github.io/Luma3DS/latest.zip"
-local stableUrl = "http://astronautlevel2.github.io/Luma3DS/release.zip"
-local hourlyDevUrl = "http://astronautlevel2.github.io/Luma3DSDev/latest.zip"
-local stableDevUrl = "http://astronautlevel2.github.io/Luma3DSDev/release.zip"
-local payload_path = "/arm9loaderhax.bin"
-local zip_path = "/Luma3DS.zip"
-local backup_path = payload_path..".bak"
-local remoteVer = "http://astronautlevel2.github.io/Luma3DS/lastVer"
-local remoteCommit = "http://astronautlevel2.github.io/Luma3DS/lastCommit"
-local remoteDevCommit = "http://astronautlevel2.github.io/Luma3DSDev/lastCommit"
-local latestCIA = "http://www.ataber.pw/u"
-local latestHBX = "http://gs2012.xyz/3ds/starupdater/index.lua" -- I'll be hosting it for now, I suppose.
+--StarUpdater-UN 
+--Unofficial StarUpdater branch by gnmmarechal.
+--Based on StarUpdater by astronautlevel.
+
+-- Colours
+local Colors =
+{
+	white = Color.new(255,255,255)
+	yellow = Color.new(255,205,66)
+	red = Color.new(255,0,0)
+	green = Color.new(55,255,0)
+}
+
+-- URLs
+local urls =
+{
+	--Luma3DS URLs
+	hourly = "http://astronautlevel2.github.io/Luma3DS/latest.zip"
+	stable = "http://astronautlevel2.github.io/Luma3DS/release.zip"
+	hourlyDev = "http://astronautlevel2.github.io/Luma3DSDev/latest.zip"
+	stableDev = "http://astronautlevel2.github.io/Luma3DSDev/release.zip"
+	--Versions String URLs
+	remoteVer = "http://astronautlevel2.github.io/Luma3DS/lastVer"
+	remoteCommit = "http://astronautlevel2.github.io/Luma3DS/lastCommit"
+	remoteDevCommit = "http://astronautlevel2.github.io/Luma3DSDev/lastCommit"	
+	--StarUpdater URLs
+	latestCIA = "http://gs2012.xyz/3ds/starupdater/latest.zep" --My branch's URL
+	--latestCIA = "http://www.ataber.pw/u" --Official URL
+	latestHBX = "http://gs2012.xyz/3ds/starupdater/index.lua"
+	--Server Latest StarUpdater Version String
+	ver = "http://gs2012.xyz/3ds/starupdater/version"
+}
+
+-- Paths
+
+local paths =
+{
+	--A9LH Payload Paths
+	payload = "/arm9loaderhax.bin"
+	--Luma3DS ZIP Path
+	zip = "/Luma3DS.zip"
+	--Payload Backup Path
+	backup = paths.payload..".bak"
+}
+
+
 local curPos = 20
 local isMenuhax = false
 local isDev = false
 local menuhaxmode, devmode = 1,2
+
 local localVer = ""
 local remoteVerNum = ""
 
+--Controls
 local pad = Controls.read()
 local oldpad = pad
 
@@ -31,37 +63,40 @@ if System.checkBuild() == 2 then
 else
 	iscia = 1
 end
-local isofficial = 1
 
-if isofficial == 0 then
-	latestCIA = "http://gs2012.xyz/3ds/starupdater/latest.zep"
-end
 --Version Info
-local sver = "1.4.0"
-local lver = "???" --This is fetched from the server
-local verserver = "http://gs2012.xyz/3ds/starupdater/version"
+
+local ver =
+{
+	--Current StarUpdater Version
+	current = "1.5.0"
+	--Latest StarUpdater version from server
+	server = "???"
+}
+
 if Network.isWifiEnabled() then
-	lver = Network.requestString(verserver)
+	ver.server = Network.requestString(urls.ver)
 end
+
 function readConfig(fileName)
     if (isMenuhax) then
-        payload_path = "/Luma3DS.dat"
-        backup_path = payload_path..".bak"
+        paths.payload = "/Luma3DS.dat"
+        paths.backup = paths.payload..".bak"
         return
     end
     if (System.doesFileExist(fileName)) then
         local file = io.open(fileName, FREAD)
-        payload_path = io.read(file, 0, io.size(file))
-        payload_path = string.gsub(payload_path, "\n", "")
-        payload_path = string.gsub(payload_path, "\r", "")
-        backup_path = payload_path..".bak"
+        paths.payload = io.read(file, 0, io.size(file))
+        paths.payload = string.gsub(paths.payload, "\n", "")
+        paths.payload = string.gsub(paths.payload, "\r", "")
+        paths.backup = paths.payload..".bak"
     elseif (not System.doesFileExist(fileName) and not isMenuhax) then
 		if System.doesFileExist("/arm9loaderhax_si.bin") and (not System.doesFileExist("/arm9loaderhax.bin")) then
-			payload_path = "/arm9loaderhax_si.bin"
+			paths.payload = "/arm9loaderhax_si.bin"
 		else
-			payload_path = "/arm9loaderhax.bin"
+			paths.payload = "/arm9loaderhax.bin"
 		end
-        backup_path = payload_path..".bak"
+        paths.backup = paths.payload..".bak"
         return
     end
 end
@@ -71,12 +106,12 @@ function restoreBackup()
     Screen.clear(TOP_SCREEN)
     Screen.waitVblankStart()
     Screen.flip()
-    if System.doesFileExist(backup_path) then
+    if System.doesFileExist(paths.backup) then
         Screen.debugPrint(5,5, "Deleting new payload...", yellow, TOP_SCREEN)
-        System.deleteFile(payload_path)
-        Screen.debugPrint(5,20, "Renaming backup to "..payload_path.."...", yellow, TOP_SCREEN)
-        System.renameFile(backup_path, payload_path)
-        Screen.debugPrint(5,35, "Press START to go back to HBL/Home menu", green, TOP_SCREEN)
+        System.deleteFile(paths.payload)
+        Screen.debugPrint(5,20, "Renaming backup to "..paths.payload.."...", yellow, TOP_SCREEN)
+        System.renameFile(paths.backup, paths.payload)
+        Screen.debugPrint(5,35, "Press START to go back to HBL/Home menu", Colors.green, TOP_SCREEN)
         while true do
             pad = Controls.read()
                 if Controls.check(pad,KEY_START) then
@@ -86,8 +121,8 @@ function restoreBackup()
             end
         end
     else
-        Screen.debugPrint(5,5, "Backup path: "..backup_path, yellow, TOP_SCREEN)
-        Screen.debugPrint(5,20, "Press START to go back to HBL/Home menu", green, TOP_SCREEN)
+        Screen.debugPrint(5,5, "Backup path: "..paths.backup, yellow, TOP_SCREEN)
+        Screen.debugPrint(5,20, "Press START to go back to HBL/Home menu", Colors.green, TOP_SCREEN)
         while true do
             pad = Controls.read()
             if Controls.check(pad,KEY_START) then
@@ -161,9 +196,9 @@ function getVer(path)
     else
         if Network.isWifiEnabled() then
         	if (not isDev) then
-            	return Network.requestString(remoteVer).."-"..Network.requestString(remoteCommit)
+            	return Network.requestString(urls.remoteVer).."-"..Network.requestString(urls.remoteCommit)
             else
-            	return Network.requestString(remoteVer).."-"..Network.requestString(remoteDevCommit)
+            	return Network.requestString(urls.remoteVer).."-"..Network.requestString(urls.remoteDevCommit)
             end
         else
             return "No connection!"
@@ -172,16 +207,16 @@ function getVer(path)
 end
 
 function path_changer()
-    local file = io.open(payload_path, FREAD)
+    local file = io.open(paths.payload, FREAD)
     local a9lh_data = io.read(file, 0, io.size(file))
     io.close(file)
     local offset = string.find(a9lh_data, "%"..unicodify("arm9loaderhax.bin"))
-    local new_path = unicodify(string.sub(payload_path,2,-1))
+    local new_path = unicodify(string.sub(paths.payload,2,-1))
     if #new_path < 74 then
         for i = 1,74-#new_path,1 do
             new_path = new_path..string.char(00)
         end
-        local file = io.open(payload_path, FWRITE)
+        local file = io.open(paths.payload, FWRITE)
         io.write(file, offset-1, new_path, 74)
         io.close(file)
     end
@@ -194,29 +229,29 @@ function update(site)
     Screen.flip()
     if Network.isWifiEnabled() then
     	Screen.debugPrint(5,5, "Downloading file...", yellow, TOP_SCREEN)
-        Network.downloadFile(site, zip_path)
-        Screen.debugPrint(5,15, "File downloaded!", green, TOP_SCREEN)
+        Network.downloadFile(site, paths.zip)
+        Screen.debugPrint(5,15, "File downloaded!", Colors.green, TOP_SCREEN)
         Screen.debugPrint(5,35, "Backing up payload", yellow, TOP_SCREEN)
-        if (System.doesFileExist(backup_path)) then
-            System.deleteFile(backup_path)
+        if (System.doesFileExist(paths.backup)) then
+            System.deleteFile(paths.backup)
         end
-        if (System.doesFileExist(payload_path)) then
-            System.renameFile(payload_path, backup_path)
+        if (System.doesFileExist(paths.payload)) then
+            System.renameFile(paths.payload, paths.backup)
         end
         if (isMenuhax == false) then
-            System.extractFromZIP(zip_path, "out/arm9loaderhax.bin", payload_path)
+            System.extractFromZIP(paths.zip, "out/arm9loaderhax.bin", paths.payload)
             Screen.debugPrint(5,50, "Moving to payload location...", yellow, TOP_SCREEN)
-            System.deleteFile(zip_path)
+            System.deleteFile(paths.zip)
             Screen.debugPrint(5,65, "Changing path for reboot patch", yellow, TOP_SCREEN)
             path_changer()
         elseif (isMenuhax == true) then
             Screen.debugPrint(5,50, "Moving to payload location...", yellow, TOP_SCREEN)
-            System.extractFromZIP(zip_path, "out/Luma3DS.dat", "/Luma3DS.dat")
-            System.deleteFile(zip_path)
+            System.extractFromZIP(paths.zip, "out/Luma3DS.dat", "/Luma3DS.dat")
+            System.deleteFile(paths.zip)
         end
-        Screen.debugPrint(5,80, "Done!", green, TOP_SCREEN)
-        Screen.debugPrint(5,95, "Press START to go back to HBL/Home menu", green, TOP_SCREEN)
-        Screen.debugPrint(5,110, "Press SELECT to reboot", green, TOP_SCREEN)
+        Screen.debugPrint(5,80, "Done!", Colors.green, TOP_SCREEN)
+        Screen.debugPrint(5,95, "Press START to go back to HBL/Home menu", Colors.green, TOP_SCREEN)
+        Screen.debugPrint(5,110, "Press SELECT to reboot", Colors.green, TOP_SCREEN)
         while true do
             pad = Controls.read()
             if Controls.check(pad,KEY_START) then
@@ -229,8 +264,8 @@ function update(site)
         end
 
     else
-        Screen.debugPrint(5,5, "WiFi is off! Please turn it on and retry!", red, TOP_SCREEN)
-        Screen.debugPrint(5,20, "Press START to go back to HBL/Home menu", red, TOP_SCREEN)
+        Screen.debugPrint(5,5, "WiFi is off! Please turn it on and retry!", Colors.red, TOP_SCREEN)
+        Screen.debugPrint(5,20, "Press START to go back to HBL/Home menu", Colors.red, TOP_SCREEN)
         while true do
             pad = Controls.read()
             if Controls.check(pad,KEY_START) then
@@ -244,7 +279,7 @@ end
 
 function init()
 	readConfig("/luma/update.cfg")
-	localVer = getVer(payload_path)
+	localVer = getVer(paths.payload)
 	remoteVerNum = getVer("remote")
 end
 
@@ -252,21 +287,21 @@ function main()
     Screen.refresh()
     Screen.clear(TOP_SCREEN)
     Screen.debugPrint(5,5, "Welcome to the StarUpdater!", yellow, TOP_SCREEN)
-    Screen.debugPrint(0, curPos, "->", white, TOP_SCREEN)
-    Screen.debugPrint(30,20, "Update stable Luma3DS", white, TOP_SCREEN)
-    Screen.debugPrint(30,35, "Update unstable Luma3DS", white, TOP_SCREEN)
-    Screen.debugPrint(30,50, "Restore a Luma3DS backup", white, TOP_SCREEN)
-    Screen.debugPrint(30,65, "Developer mode: "..getMode(devmode), white, TOP_SCREEN)
-    Screen.debugPrint(30,80, "Install mode: "..getMode(menuhaxmode), white, TOP_SCREEN)
-    Screen.debugPrint(30,95, "Go back to HBL/Home menu", white, TOP_SCREEN)
-    Screen.debugPrint(30,110, "Update the updater", white, TOP_SCREEN)
-    Screen.debugPrint(5,145, "Your Luma3DS version  : "..localVer, white, TOP_SCREEN)
-    Screen.debugPrint(5,160, "Latest Luma3DS version: "..remoteVerNum, white, TOP_SCREEN)
+    Screen.debugPrint(0, curPos, "->", Colors.white, TOP_SCREEN)
+    Screen.debugPrint(30,20, "Update stable Luma3DS", Colors.white, TOP_SCREEN)
+    Screen.debugPrint(30,35, "Update unstable Luma3DS", Colors.white, TOP_SCREEN)
+    Screen.debugPrint(30,50, "Restore a Luma3DS backup", Colors.white, TOP_SCREEN)
+    Screen.debugPrint(30,65, "Developer mode: "..getMode(devmode), Colors.white, TOP_SCREEN)
+    Screen.debugPrint(30,80, "Install mode: "..getMode(menuhaxmode), Colors.white, TOP_SCREEN)
+    Screen.debugPrint(30,95, "Go back to HBL/Home menu", Colors.white, TOP_SCREEN)
+    Screen.debugPrint(30,110, "Update the updater", Colors.white, TOP_SCREEN)
+    Screen.debugPrint(5,145, "Your Luma3DS version  : "..localVer, Colors.white, TOP_SCREEN)
+    Screen.debugPrint(5,160, "Latest Luma3DS version: "..remoteVerNum, Colors.white, TOP_SCREEN)
     if (not isMenuhax) then
-        Screen.debugPrint(5, 175, "Install directory: "..payload_path, white, TOP_SCREEN)
+        Screen.debugPrint(5, 175, "Install directory: "..paths.payload, Colors.white, TOP_SCREEN)
     end
-    Screen.debugPrint(5, 195, "Installed Updater: v."..sver, white, TOP_SCREEN)
-    Screen.debugPrint(5, 210, "Latest Updater   : v."..lver, white, TOP_SCREEN)
+    Screen.debugPrint(5, 195, "Installed Updater: v."..ver.current, Colors.white, TOP_SCREEN)
+    Screen.debugPrint(5, 210, "Latest Updater   : v."..ver.server, Colors.white, TOP_SCREEN)
     Screen.flip()
 end
 
@@ -292,15 +327,15 @@ while true do
         elseif Controls.check(pad,KEY_A) and not Controls.check(oldpad,KEY_A) then
             if (curPos == 20) then
                 if (not isDev) then
-                    update(stableUrl)
+                    update(urls.stable)
                 else
-                    update(stableDevUrl)
+                    update(urls.stableDev)
                 end
             elseif (curPos == 35) then
                 if (not isDev) then
-                    update(hourlyUrl)
+                    update(urls.hourly)
                 else
-                    update(hourlyDevUrl)
+                    update(urls.hourlyDev)
                 end
             elseif (curPos == 50) then
                 restoreBackup()
@@ -316,18 +351,18 @@ while true do
             elseif (curPos == 110) then
             	if iscia == 1 then
                 	Screen.clear(TOP_SCREEN)
-        		Screen.debugPrint(5, 5, "Downloading new CIA...", yellow, TOP_SCREEN)
-       			Network.downloadFile(latestCIA, "/Updater.CIA")
+        		Screen.debugPrint(5, 5, "Downloading new CIA...", Colors.yellow, TOP_SCREEN)
+       			Network.downloadFile(urls.latestCIA, "/Updater.CIA")
                 	sleep(2000)
-                	Screen.debugPrint(5, 20, "Installing CIA...", yellow, TOP_SCREEN)
+                	Screen.debugPrint(5, 20, "Installing CIA...", Colors.yellow, TOP_SCREEN)
                 	System.installCIA("/Updater.CIA", SDMC)
                 	System.deleteFile("/Updater.CIA")
                 	System.exit()
                 else
                 	Screen.clear(TOP_SCREEN)
-                	Screen.debugPrint(5, 5, "Downloading new script...", yellow, TOP_SCREEN)
+                	Screen.debugPrint(5, 5, "Downloading new script...", Colors.yellow, TOP_SCREEN)
 					System.deleteFile("/3ds/StarUpdater/index.lua")
-                	Network.downloadFile(latestHBX, "/3ds/StarUpdater/index.lua")
+                	Network.downloadFile(urls.latestHBX, "/3ds/StarUpdater/index.lua")
                 	System.exit()
             	end	
 
