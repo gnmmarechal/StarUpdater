0bcc3138af2b724ae7650f86f8db512cc1d06362
Gnm Software 2012 <gnmpolicemata91@gmail.com>
2016-09-25 20:51:59 +0100

    Bumped version to 1.5.2

--- index.lua
+++ index.lua
@@ -31,7 +31,7 @@ local verserver = "http://www.ataber.pw/ver" -- Unofficial URL http://gs2012.xyz
 local svrelverserver = "http://gs2012.xyz/3ds/starupdater/relver" -- Astronaut must replace this with their own URL, as done above
 
 -- Version Info
-local sver = "1.5.1"
+local sver = "1.5.2"
 local lver = "???" --This is fetched from the server
 local relver = 1 -- This is a number that is checked against the server version for mandatory updates. if svrelver > relver, StarUpdater will auto-update.
 local svrelver = 0 -- Fetched from server
